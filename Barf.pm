26ad9fed2295d04556e027029348f70ed3a78624
Michal Špaček <tupinek@gmail.com>
2011-09-07 23:01:52 +0200

    Better example in EXAMPLE doc section.

--- Barf.pm
+++ Barf.pm
@@ -64,15 +64,22 @@ IO::Barf - Barfing content to output file.
  use warnings;
 
  # Module.
+ use File::Temp qw(tempfile);
  use IO::Barf qw(barf);
 
  # Content.
  my $content = "foo\nbar\n";
 
- # Barf out.
- barf('/tmp/foo', $content);
+ # Temporary file.
+ my $temp_file = File::Temp->new->filename;
 
- # cat /tmp/foo
+ # Barf out.
+ barf($temp_file, $content);
+
+ # Print tempory file.
+ system("cat $temp_file");
+
+ # Output:
  # foo
  # bar
 
