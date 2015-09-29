48e418ebdf73b293a3265f38ae0dea27b056bf19
Michal Špaček <tupinek@gmail.com>
2015-09-29 22:49:15 +0200

    Add another usage to benchamrk.

--- Barf.pm
+++ Barf.pm
@@ -136,15 +136,19 @@ IO::Barf - Barfing content to output file.
 
  # Module.
  use Benchmark qw(cmpthese);
+ use IO::All;
  use IO::Any;
  use IO::Barf;
  use File::Slurp qw(write_file);
  use File::Temp;
+ use Path::Tiny;
 
  # Temporary files.
  my $temp1 = File::Temp->new->filename;
  my $temp2 = File::Temp->new->filename;
  my $temp3 = File::Temp->new->filename;
+ my $temp4 = File::Temp->new->filename;
+ my $temp5 = File::Temp->new->filename;
 
  # Some data.
  my $data = 'x' x 1000;
@@ -155,6 +159,10 @@ IO::Barf - Barfing content to output file.
                  write_file($temp3, $data);
                  unlink $temp3;
          },
+         'IO::All' => sub {
+                 $data > io($temp4);
+                 unlink $temp4;
+         },
          'IO::Any' => sub {
                  IO::Any->spew($temp2, $data);
                  unlink $temp2;
@@ -163,13 +171,71 @@ IO::Barf - Barfing content to output file.
                  barf($temp1, $data);
                  unlink $temp1;
          },
+         'Path::Tiny' => sub {
+                 path($temp5)->spew($data);
+                 unlink $temp5;
+         },
  });
 
  # Output like this:
- #                Rate     IO::Any File::Slurp    IO::Barf
- # IO::Any      6382/s          --        -24%        -48%
- # File::Slurp  8367/s         31%          --        -32%
- # IO::Barf    12268/s         92%         47%          --
+ #                Rate  Path::Tiny     IO::Any     IO::All File::Slurp    IO::Barf
+ # Path::Tiny   3210/s          --        -17%        -51%        -85%        -91%
+ # IO::Any      3859/s         20%          --        -41%        -82%        -89%
+ # IO::All      6574/s        105%         70%          --        -70%        -81%
+ # File::Slurp 21615/s        573%        460%        229%          --        -39%
+ # IO::Barf    35321/s       1000%        815%        437%         63%          --
+
+=head1 EXAMPLE4
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Module.
+ use Benchmark qw(cmpthese);
+ use File::Temp;
+
+ # Temporary files.
+ my $temp1 = File::Temp->new->filename;
+ my $temp2 = File::Temp->new->filename;
+ my $temp3 = File::Temp->new->filename;
+ my $temp4 = File::Temp->new->filename;
+ my $temp5 = File::Temp->new->filename;
+
+ # Some data.
+ my $data = 'x' x 1000;
+
+ # Benchmark (10s).
+ cmpthese(-10, {
+         'File::Slurp' => sub {
+                 require File::Slurp;
+                 File::Slurp::write_file($temp3, $data);
+                 unlink $temp3;
+         },
+         'IO::All' => sub {
+                 require IO::All;
+                 $data > io($temp4);
+                 unlink $temp4;
+         },
+         'IO::Any' => sub {
+                 require IO::Any;
+                 IO::Any->spew($temp2, $data);
+                 unlink $temp2;
+         },
+         'IO::Barf' => sub {
+                 require IO::Barf;
+                 barf($temp1, $data);
+                 unlink $temp1;
+         },
+         'Path::Tiny' => sub {
+                 require Path::Tiny;
+                 path($temp5)->spew($data);
+                 unlink $temp5;
+         },
+ });
+
+ # Output like this:
+ # TODO
 
 =head1 DEPENDENCIES
 
